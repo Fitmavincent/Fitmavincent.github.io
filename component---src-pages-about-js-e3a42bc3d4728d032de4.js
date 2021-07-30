9986eb6b151c7990ceac0e02fd9835ff05ce98c4
VincentLiang <Vincent.Liang@tandm.ai>
2021-07-30 17:22:11 +1000

    Updates

---
+++ component---src-pages-about-js-e3a42bc3d4728d032de4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvince_blog=self.webpackChunkvince_blog||[]).push([[682],{5518:function(e,t,n){n.r(t);var l=n(7294),a=(n(3992),n(813)),c=n(6179);t.default=function(e){var t=e.data,n=e.location,i=t.site.siteMetadata.title;return l.createElement(a.Z,{location:n,title:i},l.createElement(c.Z,{title:"About"}),l.createElement("p",null,"About Vince. He is no rock star, just a ",l.createElement("strong",null,"Developer")))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-e3a42bc3d4728d032de4.js.map

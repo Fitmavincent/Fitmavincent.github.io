d4ea33377d6b6e7c37640fd377c8a065f351c746
Vincent Liang <7020812+Fitmavincent@users.noreply.github.com>
2022-02-13 11:44:55 +1000

    Add about images and randomizer

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,6 +6,7 @@
  */
 
 import React from "react"
+import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
@@ -37,6 +38,12 @@ const Bio = () => {
     }
   `)
 
+  const linkStyle = {
+    boxShadow: `none`,
+    color: `inherit`,
+    display: `flex`,
+  }
+
   const { author, social } = data.site.siteMetadata
   return (
     <div
@@ -51,29 +58,33 @@ const Bio = () => {
           display: `flex`,
         }}
       >
-        <Image
-          fixed={data.avatar.childImageSharp.fixed}
-          alt={author.name}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            minWidth: 50,
-            borderRadius: `100%`,
-          }}
-          imgStyle={{
-            borderRadius: `50%`,
-          }}
-        />
-        <p>
-          This is <strong>{author.name}</strong>
-          <br></br>
-          {author.summary}
-          {` `}
-          {/* <a href={`https://twitter.com/${social.twitter}`}>
-            You should follow him on Twitter
-          </a> */}
-        </p>
-        
+        <Link
+          style={linkStyle}
+          to={`/about`}
+        >
+          <Image
+            fixed={data.avatar.childImageSharp.fixed}
+            alt={author.name}
+            style={{
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              minWidth: 50,
+              borderRadius: `100%`,
+            }}
+            imgStyle={{
+              borderRadius: `50%`,
+            }}
+          />
+          <p>
+            This is <strong>{author.name}</strong>
+            <br></br>
+            {author.summary}
+            {` `}
+            {/* <a href={`https://twitter.com/${social.twitter}`}>
+              You should follow him on Twitter
+            </a> */}
+          </p>
+        </Link>
       </div>
       <Toggler></Toggler>
     </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import Header from "../components/header"
 
 import Layout from "../components/layout"
@@ -7,11 +8,21 @@ import SEO from "../components/seo"
 
 const About = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
+  const aboutImageOne = data.aboutOne?.childImageSharp?.fluid;
+  const aboutImageTwo = data.aboutTwo?.childImageSharp?.fluid;
+  const aboutImageThree = data.aboutThree?.childImageSharp?.fluid;
+  const aboutImages = [aboutImageOne, aboutImageTwo, aboutImageThree];
+  const aboutImage = aboutImages[Math.floor(Math.random()*aboutImages.length)];
 
   return (    
     <Layout location={location} title={siteTitle}>
       <SEO title="About" />      
       <p>About Vince. He is no rock star, just a <strong>Developer</strong></p>
+      <Image
+        className="splash-img"
+        fluid={aboutImage}
+        alt="about-iamge"
+      />
     </Layout>
   )
 }
@@ -20,6 +31,27 @@ export default About
 
 export const pageQuery = graphql`
   query {
+    aboutOne: file(absolutePath: { regex: "/about-pic-1.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 590, maxHeight: 800) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    aboutTwo: file(absolutePath: { regex: "/about-pic-2.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 790, maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    aboutThree: file(absolutePath: { regex: "/about-pic-3.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 790, maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
     site {
       siteMetadata {
         title
